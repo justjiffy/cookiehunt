32b4fad393634b70484c85db02f15b4606d73d8c
Juli Emmel <justjiffy@gmail.com>
2015-11-05 14:44:51 -0800

    set index route to application#index

--- cookiehunt/db/migrate/20151105221113_create_users.rb
+++ cookiehunt/db/migrate/20151105221113_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string] :[name[
-      t.string] :photo[
-      t.string] :address[
-      t.boolean] :monster[
-      t.boolean] :baker[
-      t.boolean]] :waiver[
+      t.string :name
+      t.string :photo
+      t.string :address
+      t.boolean :monster
+      t.boolean :baker
+      t.boolean :waiver
 
       t.timestamps null: false
     end
